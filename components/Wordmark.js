b016d857dc450bbc5b2319406848628eaca55b5e
markflowers <mark.s.flowers@gmail.com>
2018-02-01 14:32:23 -0500

    Fix className property error

--- components/Wordmark.js
+++ components/Wordmark.js
@@ -1,2 +1,2 @@
 // @flow
-export default () => <img class="svgIcon-use" height="50" src="https://skookum.com/assets/images/logo-type-white.svg" />
+export default () => <img className="svgIcon-use" height="50" src="https://skookum.com/assets/images/logo-type-white.svg" />
